14f4a39a5c01d29b5e9272e842413b2ac476cba6
matiux <m.galacci@gmail.com>
2015-02-05 00:38:42 +0100

    modificato serializer custom di fractal con chiavi data e meta

--- src/Rufy/RestApiBundle/Transformer/Fractal/Serializer/CustomSerializer.php
+++ src/Rufy/RestApiBundle/Transformer/Fractal/Serializer/CustomSerializer.php
@@ -5,25 +5,5 @@ use League\Fractal\Serializer\DataArraySerializer;
 
 class CustomSerializer extends DataArraySerializer
 {
-    /**
-     * Serialize a collection
-     *
-     * @param  array  $data
-     * @return array
-     **/
-    public function collection($resourceKey, array $data)
-    {
-        return array('data' => $data, 'meta' => []);
-    }
 
-    /**
-     * Serialize an item
-     *
-     * @param  array  $data
-     * @return array
-     **/
-    public function item($resourceKey, array $data)
-    {
-        return array('data' => $data, 'meta' => []);
-    }
 }
