33a376313e69ea4b763ef7c2d3bbbbfe20bef611
matteo <matteo@adriasadriasonline.it>
2015-02-18 18:33:07 +0100

    validazine post reservation

--- src/Rufy/RestApiBundle/RufyRestApiBundle.php
+++ src/Rufy/RestApiBundle/RufyRestApiBundle.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  * Provare a creare un controller come servizio con metodi condivisi
  * Rifattorizzare l'utilizzo dell'handler nei controller
  * Base Voter per metodi condivisi
+ * Forse rimuovere ResourceNotFoundException
  * Allineare Test
  *      - PhpSpec per unitari
  *      - Behat per funzionali
