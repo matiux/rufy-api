2ae6ea7a8bcf4c25038c1d6b4ea70dc36f5bd3fe
matteo <matteo@adriasadriasonline.it>
2015-03-04 18:26:26 +0100

    appunti

--- src/Rufy/RestApiBundle/RufyRestApiBundle.php
+++ src/Rufy/RestApiBundle/RufyRestApiBundle.php
@@ -6,8 +6,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  * TODO
  * Provare a creare un controller come servizio con metodi condivisi
  * Rifattorizzare l'utilizzo dell'handler nei controller??
+ * Controllo Servizi / Injection con firma con interfacce
  * Voter per associazione opzioni_area alle aree in fase di salvataggio prenotazione (AreaRepository::hasOptions()) - Finire
- * Il posto dovrebbe restituire 201
+ * Il post dovrebbe restituire 201
  * Allineare Test
  *      - PhpSpec per unitari
  *      - Behat per funzionali - aggiungere casi con errori 403 - 404 - 400 ecc
