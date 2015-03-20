58d9062dfcaa61d16d36899a4430d241ef93f667
matiux <m.galacci@gmail.com>
2015-03-20 01:28:28 +0100

    prova voter opzioni prenotazione

--- src/Rufy/RestApiBundle/Repository/AreaRepository.php
+++ src/Rufy/RestApiBundle/Repository/AreaRepository.php
@@ -3,13 +3,28 @@
 use Doctrine\ORM\EntityRepository;
 use Rufy\RestApiBundle\Entity\Area;
 use Rufy\RestApiBundle\Entity\Reservation;
+use Rufy\RestApiBundle\Entity\ReservationOption;
 
 class AreaRepository extends EntityRepository
 {
     public function hasOptions(Reservation $reservation)
     {
-        //$areaOptions            = $reservation->getArea()->getAreaOptions();
+        $areaOptions            = $reservation->getArea()->getAreaOptions();
+
         $areaId                 = $reservation->getArea()->getId();
+
+        /**
+         * Ciclo le opzioni appartenenti all'area utilizzata per la prenotazione
+         */
+        foreach ($areaOptions as $opt) {
+            /**
+             * @var $opt ReservationOption
+             */
+            $slug = $opt->getSlug();
+
+        }
+
+
         /**
          * @var $area Area;
          */
